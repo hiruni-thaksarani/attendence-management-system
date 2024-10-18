0dbab745223462f10e71a62b7008cba77a97a790
hiruni-thaksarani <hirunit@web3genes.com>
2024-10-18 17:13:32 +0530

    Update ContractInstance.js

--- src/contract/ContractInstance.js
+++ src/contract/ContractInstance.js
@@ -5,7 +5,7 @@ const getContractInstance = async () => {
   if (typeof window.ethereum !== 'undefined') {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
     const web3 = new Web3(window.ethereum);
-    const contractAddress = '0x98202fd616c8e965ea5d1180231fdf087caeb98f';
+    const contractAddress = '0xe1284b8931c83396dE95473576ceC8E27D2b51b6';
     const acc = new web3.eth.Contract(pocABI, contractAddress);
     console.log('acc',acc.defaultChain);
     return acc;
